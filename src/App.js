91fdfb58404fa969317960439340cbc4a8da85b0
hal01001k <hiteshdaksh@yahoo.co.uk>
2021-01-27 23:36:54 +0530

    components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Main from './components/MainComponent'
 function App() {
   return (
     <div>
-     Welcome comp
     <Main/>
     </div>
   );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,13 @@
 import React,{Component} from 'react'
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 class Main extends Component{
     render(){
         return (
             <div>
-Main comp
-
+                <Header/>
+            Main comp
+                <Footer/>
             </div>
 
         );
